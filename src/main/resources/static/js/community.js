d41c0270f190b9214fcb826ba7ef872505cd9090
hoby <2023622784@qq.com>
2020-10-31 12:27:29 +0800

    实现问题回复之不刷新页面登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -15,6 +15,14 @@ function post() {
             if (response.code==200){
                 $("#comment_section").hide();
             }else {
+                if (response.code==2003){
+                   var isAccepted= confirm(response.message);
+                   if (isAccepted){
+                       window.open("https://gitee.com/oauth/authorize?client_id=8c683c308369fe66014bb08f63346c2e85c9a48dd0d99539beb0739c1b2fe745&redirect_uri=http://localhost:8887/callback&response_type=code");
+                       window.localStorage.setItem("closable", true);
+
+                   }
+                }
                 alert(response.message);
             }
             console.log(response)
