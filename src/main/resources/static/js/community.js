d02183cd05265ebbfc32ffb3b67f0096dc8b198c
hoby <2023622784@qq.com>
2020-10-31 18:48:30 +0800

    优化回复功能和问题详情页面

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -2,6 +2,11 @@
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
+
+    if (!content){
+        alert("不能回复空内容~~~~");
+        return ;
+    }
     $.ajax({
         type:"POST",
         url:"/comment",
@@ -13,6 +18,7 @@ function post() {
         }),
         success:function (response) {
             if (response.code==200){
+                window.location.reload();
                 $("#comment_section").hide();
             }else {
                 if (response.code==2003){
