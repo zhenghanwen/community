161211a222de23fce001eed610346f4944addd54
hoby <2023622784@qq.com>
2020-11-01 20:07:08 +0800

    实现二级评论功能之页面和接口实现

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,8 +1,11 @@
-
+/*
+* 提交回复*/
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
-
+    comment2target(questionId,1,content);
+}
+function comment2target(targetId ,type,content) {
     if (!content){
         alert("不能回复空内容~~~~");
         return ;
@@ -12,22 +15,22 @@ function post() {
         url:"/comment",
         contentType:"application/json",
         data:JSON.stringify({
-            "parentId":questionId,
+            "parentId":targetId,
             "content":content,
-            "type":1
+            "type":type
         }),
         success:function (response) {
-            if (response.code==200){
+            if (response.code===200){
                 window.location.reload();
                 $("#comment_section").hide();
             }else {
-                if (response.code==2003){
-                   var isAccepted= confirm(response.message);
-                   if (isAccepted){
-                       window.open("https://gitee.com/oauth/authorize?client_id=8c683c308369fe66014bb08f63346c2e85c9a48dd0d99539beb0739c1b2fe745&redirect_uri=http://localhost:8887/callback&response_type=code");
-                       window.localStorage.setItem("closable", true);
+                if (response.code===2003){
+                    var isAccepted= confirm(response.message);
+                    if (isAccepted){
+                        window.open("https://gitee.com/oauth/authorize?client_id=8c683c308369fe66014bb08f63346c2e85c9a48dd0d99539beb0739c1b2fe745&redirect_uri=http://localhost:8887/callback&response_type=code");
+                        window.localStorage.setItem("closable", true);
 
-                   }
+                    }
                 }
                 alert(response.message);
             }
@@ -35,8 +38,52 @@ function post() {
         },
         dataType:"json"
     });
-    console.log(questionId);
-    console.log(content);
+}
 
+function comment(e) {
+    var commentId = e.getAttribute("data-id");
+    var content = $("#input-"+commentId).val();
+    comment2target(commentId,2,content);
+
+}
+/*
+* 展开二级评论*/
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-"+id);
+
+    //获取一下二级评论的展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse){
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    }else {
+        $.getJSON( "/comment/"+id, function( data ) {
+            console.log(data);
+            var commentBody = $("comment-body-"+id);
+            var items = [];
+            $.each( data.data, function(comment) {
+                var c=$("<div/>",{
+                    "class":"col-lg-12 col-md-12 col-sm-12 col-xs-12 comments",
+                    html: comment.content
+                });
+                items.push(c);
+            });
+
+            commentBody.append("<div/>",{
+                "class":"col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse sub-comments",
+                "id":"comment-"+id,
+                html: items.join("")
+            });
+
+            //展开二级评论
+            comments.addClass("in");
+            //标记二级评论张开状态
+            e.setAttribute("data-collapse","in");
+            e.classList.add("active");
+        });
+    }
 }
 
